397d9cd99ef74c34acfd127f66be13043d0ee925
Isuru Anuradha <Isuruanuradha99@gmail.com>
2019-03-18 14:26:29 +0800

    firat commit

---
+++ Singtel_Isuru/src/test/java/com/test/Singtel_Isuru/SingtelIsuruApplicationTests.java
@@ -0,0 +1,16 @@
+package com.test.Singtel_Isuru;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SingtelIsuruApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
---
+++ src/main/java/com/test/Singtel_Isuru/SingtelIsuruApplication.java
@@ -0,0 +1,13 @@
+package com.test.Singtel_Isuru;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SingtelIsuruApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SingtelIsuruApplication.class, args);
+	}
+
+}
